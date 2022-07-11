ca610856904fa5925d32c413a5882460c846a07d
noppytinto <34626569+noppytinto@users.noreply.github.com>
2022-07-11 16:58:22 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -1,59 +1,117 @@
-/////////////////////////////
-// init stuff
-/////////////////////////////
+/////////////////////////////////////////
+// WEBPACK IMPORTS
+/////////////////////////////////////////
 import './styles/style.scss';
-import {animate, layout} from "./scripts/card";
-
-
-/////////////////////////////////////////
-// dom elements
-/////////////////////////////////////////
-
-
-/////////////////////////////////////////
-// declarations
-/////////////////////////////////////////
 
 
 /////////////////////////////////////////
 // main()
 /////////////////////////////////////////
-console.log('hello world');
-const row1code = [
-    {
-        text: 'var ',
-        style: {
-            color: '#F18200',
-            fontStyle: 'italic',
-        }
-    },
-    {text: 'result ', style: {}},
-    {text: '= ', style: {}},
-    {text: 'whoIs', style: {color: '#BE6CFF'}},
-    {text: '(', style: {color: '#FFB800'}},
-    {text: '\'Francesco Scutellaro\'', style: {color: '#00A944'}},
-    {text: ')', style: {color: '#FFB800'}},
-    {text: ';', style: {}},
-]
+main();
 
-const row2code = [
-    {text: 'console.', style: {}},
-    {text: 'log', style: {color: '#BE6CFF'}},
-    {text: '(', style: {color: '#FFB800'}},
-    {text: 'result', style: {}},
-    {text: ')', style: {color: '#FFB800'}},
-    {text: ';', style: { }},
-]
 
-const rowLoading = [
-    {text: '...', style: {color: '#e1e1e1', fontWeight: 'bold'}},
-]
+/////////////////////////////////////////
+// FUNCTIONS
+/////////////////////////////////////////
+async function main() {
+    console.log('hello world');
 
-const row3code = [
-    {text: 'WEB DEVELOPER :)', style: {color: '#e1e1e1', fontWeight: 'bold'}},
-]
+    // const container = document.querySelector('.home__typing-container')
+    const breakElement = document.createElement('br');
 
-start();
+    const stringColor = '#00BC4B';
+    const keywordColor = '#F18200';
+    const symbolColor = '#FFB800';
+    const functionColor = '#BE6CFF';
+    const terminalTextColor = '#e1e1e1';
+
+    const row1_text = [
+        {
+            text: 'var ',
+            style: {
+                color: keywordColor,
+                fontStyle: 'italic',
+            }
+        },
+        {text: 'result ', style: {}},
+        {text: '= ', style: {}},
+        {text: 'whoIs', style: {color: functionColor}},
+        {text: '(', style: {color: symbolColor}},
+        {text: '\'Francesco Scutellaro\'', style: {color: stringColor}},
+        {text: ')', style: {color: symbolColor}},
+        {text: ';', style: {}},
+    ]
+
+    const row2_text = [
+        {text: 'console.', style: {}},
+        {text: 'log', style: {color: functionColor}},
+        {text: '(', style: {color: symbolColor}},
+        {text: 'result', style: {}},
+        {text: ')', style: {color: symbolColor}},
+        {text: ';', style: { }},
+    ]
+
+    const rowLoading = [
+        {text: '...', style: {color: terminalTextColor, fontWeight: 'bold'}},
+    ]
+
+    const row3_text = [
+        {text: 'WEB DEVELOPER :)', style: {color: terminalTextColor, fontWeight: 'bold'}},
+    ]
+
+
+    ///////////////////////
+    // logic
+    ///////////////////////
+    await typeCode(row1_text, '#row-1');
+    insertAfter(breakElement, document.querySelector('#row-1'));
+    await typeCode(row2_text, '#row-2');
+    await typeCode(rowLoading, '#row-loading', 300);
+    await typeCode(row3_text, '#row-3', 5);
+}
+
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
+
+async function typeCode(code, elementRef, delay = 50) {
+    for (const word of code) {
+        const span = document.createElement('span');
+        document.querySelector(elementRef).append(span);
+
+        applyStyle(word.style, span);
+
+        await typeText(word.text, span, delay)
+    }
+}
+
+function applyStyle(styleProperties, elementNode) {
+    for (const [key, value] of Object.entries(styleProperties)) {
+        elementNode.style[`${key}`] = value;
+        console.log(key);
+    }
+}
+
+async function typeText(text, elementNode, delay = 50) {
+    const letters = text.split("");
+
+    for (const letter of letters) {
+        elementNode.append(letter);
+        await waitForMs(delay);
+    }
+}
+
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+
+/////////////////////////////////////////
+// MISC
+/////////////////////////////////////////
+// import {animate, layout} from "./scripts/card";
+
 // const cardsToGlance = 3;
 // const depth = 1;
 // const cards = document.querySelectorAll('.card');
@@ -71,59 +129,3 @@ start();
 // });
 
 //
-
-
-
-
-/////////////////////////////////////////
-// functions
-/////////////////////////////////////////
-const container = document.querySelector('.home__typing-container')
-const breakElement = document.createElement('br');
-
-async function start() {
-    await typeCode(row1code, '#row-1');
-    insertAfter(document.createElement('br'), document.querySelector('#row-1'));
-    await typeCode(row2code, '#row-2');
-    await typeCode(rowLoading, '#row-loading', 500);
-    await typeCode(row3code, '#row-3', 10);
-}
-
-function insertAfter(newNode, existingNode) {
-    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
-}
-
-
-async function typeCode(code, elementRef, delay = 50) {
-    for (const word of code) {
-        const span = document.createElement('span');
-        document.querySelector(elementRef).append(span);
-
-        applyStyle(word.style, span);
-
-        await typeSentence(word.text, span, delay)
-    }
-}
-
-function applyStyle(styleProperties, elementNode) {
-    for (const [key, value] of Object.entries(styleProperties)) {
-        elementNode.style[`${key}`] = value;
-        console.log(key);
-    }
-}
-
-async function typeSentence(text, elementNode, delay = 50) {
-    const letters = text.split("");
-    for (const letter of letters) {
-        elementNode.append(letter);
-        await waitForMs(delay);
-    }
-}
-
-function waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-function addCursor(container) {
-    const cursor = document.cre
-}
